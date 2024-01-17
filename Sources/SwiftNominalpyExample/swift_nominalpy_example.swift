db93afde768a85c2eb83c539e83db3f839868f29
Jordan Nowell <jordannowell94@gmail.com>
2024-01-17 15:19:43 +1100

    Initial NominalPy Import Test
    Imports the NominalPy Constants to print the Equatorial radius of Earth

--- Sources/SwiftNominalpyExample/swift_nominalpy_example.swift
+++ Sources/SwiftNominalpyExample/swift_nominalpy_example.swift
@@ -1,12 +1,14 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-import PythonKit 
+import PythonKit
 
 public struct SwiftNominalpyExample {
 
-	public static func run() {
-		let sys = Python.import("sys")
-		print("Python \(sys.version_info.major).\(sys.version_info.minor)")
-	}
+    public static func run() {
+        let nominalpy = Python.import("nominalpy")
+        let constants = Python.import("nominalpy.maths.constants")
+        print(constants.EARTH_REQ)
+    }
 }
+
