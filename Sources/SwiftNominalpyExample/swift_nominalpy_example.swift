ccd6b64e8b583007004b0805a17b22d17c427f80
Jordan Nowell <jordannowell94@gmail.com>
2024-01-22 12:45:39 +1100

    Reformatted the example to tidy it up

--- Sources/SwiftNominalpyExample/swift_nominalpy_example.swift
+++ Sources/SwiftNominalpyExample/swift_nominalpy_example.swift
@@ -1,5 +1,17 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+/*
+                    [ NOMINAL SYSTEMS ]
+This code is developed by Nominal Systems to aid with communication
+to the public API. All code is under the the license provided along
+with the 'nominalpy' module. Copyright Nominal Systems, 2024.
+
+This example shows how the NominalPy module can be accessed in
+Swift Via PythonKit.
+
+The scenario is of a spacecraft with a solar panel and a
+sun pointing ADCS system that orients the spacecraft to face
+its solar panel towards the sun, using a flight software chain.
+The solar panel power and pointing error are exported as text files.
+*/
 
 import Foundation
 import PythonKit
@@ -8,118 +20,138 @@ public struct SwiftNominalpyExample {
 
     public static func run()
     {
+        // Import Required NominalPy Modules
+        let nominalpy = Python.import("nominalpy")
+        let credentials = Python.import("nominalpy.connection.credentials")
+        let simulation = Python.import("nominalpy.objects.simulation")
+        let astro = Python.import("nominalpy.maths.astro")
 
+        // Import Required Python Modules
+        let datetime = Python.import("datetime")
+        let np = Python.import("numpy")
+        let json = Python.import("json")
+
+        let tick_size=0.05
+        let tick_iterations=20000
+		let data_sample_rate=5.0
+
+        // Set up the API access
         let url = "https://api.nominalsys.com"
         let port = Python.None
 
         if let apiKey = ProcessInfo.processInfo.environment["NOMINAL_API_KEY"]
         {
-            print("Nominal API Key Recieved")
+            print("Nominal API Key Imported")
 
-            let credentials = Python.import("nominalpy.connection.credentials")
+            // Connect to the Nominal API
             let auth: PythonObject = credentials.Credentials(url,port,apiKey)
-
-            let simulation = Python.import("nominalpy.objects.simulation")
             let sim: PythonObject = simulation.Simulation(auth)
+            print("Connected to Nominal API")
 
-            print("Pre Tick sim.get_time: ",sim.get_time())
-
-            let nominalpy = Python.import("nominalpy")
-            let types = nominalpy.types
-
-            let datetime = Python.import("datetime")
-
+            print("Configuring the Simulation")
             //Configure the Universe with an epoch
+            let types = nominalpy.types
             let universe = sim.get_system(types.UNIVERSE, Epoch: datetime.datetime(2022, 1, 1))
-
-            let astro = Python.import("nominalpy.maths.astro")
 
             //Compute the orbit from the Keplerian elements to a state vector of (position, velocity)
             let orbit: PythonObject = astro.classical_to_vector_elements(6671, inclination: 35, true_anomaly: 16)
 
-            let np = Python.import("numpy")
-
-            //Adds the spacecraft
+            //Add the spacecraft
             let spacecraft: PythonObject = sim.add_component(types.SPACECRAFT,
-            TotalMass: 750.0,
+                TotalMass: 750.0,
                 TotalCenterOfMass: np.array([0, 0, 0]),
-            TotalMomentOfInertia: np.array([[900, 0, 0], [0, 800, 0], [0, 0, 600]]),
+                TotalMomentOfInertia: np.array([[900, 0, 0], [0, 800, 0], [0, 0, 600]]),
                 Position: orbit[0],
                 Velocity: orbit[1],
                 AttitudeRate: np.array([0.2, 0.1, 0.05]))
 
-            //Adds reaction wheels
-            let reactionWheels: PythonObject = sim.add_component("ReactionWheelArray",spacecraft)
+            //Add reaction wheels
+            let reaction_wheels: PythonObject = sim.add_component("ReactionWheelArray",spacecraft)
 
-            let rw1: PythonObject = sim.add_component("ReactionWheel",reactionWheels,WheelSpinAxis_B:np.array([1,0,0]))
-            let rw2: PythonObject = sim.add_component("ReactionWheel",reactionWheels,WheelSpinAxis_B:np.array([0,1,0]))
-            let rw3: PythonObject = sim.add_component("ReactionWheel",reactionWheels,WheelSpinAxis_B:np.array([0,0,1]))
+            let rw1: PythonObject = sim.add_component("ReactionWheel",reaction_wheels,WheelSpinAxis_B:np.array([1,0,0]))
+            let rw2: PythonObject = sim.add_component("ReactionWheel",reaction_wheels,WheelSpinAxis_B:np.array([0,1,0]))
+            let rw3: PythonObject = sim.add_component("ReactionWheel",reaction_wheels,WheelSpinAxis_B:np.array([0,0,1]))
 
-            //Adds a simple Navigator
+            //Add a simple Navigator
             let navigator: PythonObject = sim.add_component("SimpleNavigator",spacecraft)
 
-            //Adds a Solar Panel
-            let solarPanel: PythonObject = sim.add_component("SolarPanel",spacecraft,Area:0.01,Efficiency:0.23)
+            //Add a Solar Panel
+            let solar_panel: PythonObject = sim.add_component("SolarPanel",spacecraft,Area:0.01,Efficiency:0.23)
 
-            //Adds in Sun Safe Pointing
-            let sunPointFSW: PythonObject = sim.add_component("SunSafePointingSoftware",spacecraft,
+            //Add in Sun Safe Pointing
+            let sun_point_fsw: PythonObject = sim.add_component("SunSafePointingSoftware",spacecraft,
                 minUnitMag:0.001,
                 SmallAngle:0.001,
-                SunBodyVector:solarPanel.get_value("LocalUp"),
+                SunBodyVector:solar_panel.get_value("LocalUp"),
                 Omega_RN_B:np.array([0,0,0]),
                 SunAxisSpinRate:0.0,
                 In_NavAttMsg:navigator.get_value("Out_NavAttMsg"),
                 In_SunDirectionMsg:navigator.get_value("Out_NavAttMsg"))
 
             //Add in the MRP feedback software
-            let mrpFeedbackFsw: PythonObject = sim.add_component("MRPFeedbackSoftware", spacecraft,
+            let mrp_feedback_fsw: PythonObject = sim.add_component("MRPFeedbackSoftware", spacecraft,
                 K:3.5,
                 P:30.0,
                 Ki:-1.0,
                 IntegralLimit:-20,
-                In_RWSpeedMsg:reactionWheels.get_value("Out_RWSpeedMsg"),
-                In_RWArrayConfigMsg:reactionWheels.get_value("Out_RWArrayConfigMsg"),
-                In_AttGuidMsg:sunPointFSW.get_value("Out_AttGuidMsg"),
+                In_RWSpeedMsg:reaction_wheels.get_value("Out_RWSpeedMsg"),
+                In_RWArrayConfigMsg:reaction_wheels.get_value("Out_RWArrayConfigMsg"),
+                In_AttGuidMsg:sun_point_fsw.get_value("Out_AttGuidMsg"),
                 In_VehicleConfigMsg:spacecraft.get_value("Out_VehicleConfigMsg"))
 
             //Add in the motor torque software
-            let motorTorqueFsw: PythonObject = sim.add_component("ReactionWheelMotorTorqueSoftware",spacecraft,
-                In_CmdTorqueBodyMsg:mrpFeedbackFsw.get_value("Out_CmdTorqueBodyMsg"),
-                In_RWArrayConfigMsg:reactionWheels.get_value("Out_RWArrayConfigMsg"))
+            let motor_torque_fsw: PythonObject = sim.add_component("ReactionWheelMotorTorqueSoftware",spacecraft,
+                In_CmdTorqueBodyMsg:mrp_feedback_fsw.get_value("Out_CmdTorqueBodyMsg"),
+                In_RWArrayConfigMsg:reaction_wheels.get_value("Out_RWArrayConfigMsg"))
 
             //Connect up to the reaction wheels
-            reactionWheels.set_value("In_ArrayMotorTorqueMsg", motorTorqueFsw.get_value("Out_ArrayMotorTorqueMsg"))
+            reaction_wheels.set_value("In_ArrayMotorTorqueMsg", motor_torque_fsw.get_value("Out_ArrayMotorTorqueMsg"))
 
             //Register some messages to be stored in a database
-            spacecraft.get_message("Out_EclipseMsg").subscribe(5.0)
-            navigator.get_message("Out_NavAttMsg").subscribe(5.0)
-            sunPointFSW.get_message("Out_AttGuidMsg").subscribe(5.0)
-            solarPanel.get_message("Out_PowerSourceMsg").subscribe(5.0)
-            reactionWheels.get_message("Out_RWSpeedMsg").subscribe(5.0)
+            spacecraft.get_message("Out_EclipseMsg").subscribe(data_sample_rate)
+            navigator.get_message("Out_NavAttMsg").subscribe(data_sample_rate)
+            sun_point_fsw.get_message("Out_AttGuidMsg").subscribe(data_sample_rate)
+            solar_panel.get_message("Out_PowerSourceMsg").subscribe(data_sample_rate)
+            reaction_wheels.get_message("Out_RWSpeedMsg").subscribe(data_sample_rate)
+
+            print("Simulation Configured")
 
             //Execute the simulation to be ticked
-            sim.tick(0.05,2000)
+            print("Tick the simulation for:",tick_iterations,"steps with a step size of", tick_size,"(",tick_size*Double(tick_iterations),"seconds )" )
 
-            print("Post Tick sim.get_time: ",sim.get_time())
-            print(solarPanel.get_message("Out_PowerSourceMsg").fetch("Power"))
-
-			do 
+			for i in 0..<10
 			{
-            	if let outputPath = ProcessInfo.processInfo.environment["NOMINAL_API_OUTPUT_PATH"]
-            	{
-                	let json = Python.import("json")
-                	let data = String(json.dumps(solarPanel.get_message("Out_PowerSourceMsg").fetch("Power")))!
-                	try data.write(toFile: "\(outputPath)/Data_Solarpanel_Power.txt", atomically: true, encoding: .utf8)
-            	}
-			} 
-			catch 
-			{
-				print("Error writing data to file")
+            	sim.tick(tick_size,tick_iterations/10)
+				print("Percentage Progress:", i*10)
 			}
+			print("Percentage Progress: 100")
+
+            //Get the data from the simulation
+            print("Exporting data")
+
+            do
+            {
+                if let outputPath = ProcessInfo.processInfo.environment["NOMINAL_API_OUTPUT_PATH"]
+                {
+                    let data_power = String(json.dumps(solar_panel.get_message("Out_PowerSourceMsg").fetch("Power")))!
+                    try data_power.write(toFile: "\(outputPath)/Data_solar_panel_Power.txt", atomically: true, encoding: .utf8)
+                    print("Power Data exported")
+                }
+                else
+                {
+                    print("OUTPUT_PATH Environment variable not set.")
+                }
+            }
+            catch
+            {
+                print("Error writing data to file")
+            }
+
+            print("Simulation Complete")
         }
         else
         {
-            print("Environment variable not set.")
+            print("NOMINAL_API_KEY Environment Variable not set.")
         }
     }
 }
